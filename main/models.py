30f781d24cac6e123e93ae9150532d2d04aa3564
hello <azamatbaatay@gmail.com>
2022-04-05 23:57:08 +0600

    Add serializers

--- main/models.py
+++ main/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
     text = models.TextField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -23,3 +23,5 @@ class Post(models.Model):
 class PostImage(models.Model):
     image = models.ImageField(upload_to='posts', blank=True, null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+
+
